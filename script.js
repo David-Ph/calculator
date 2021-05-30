4574cd817cf0c068ad9ef3f980c6cf3092ab57ef
David Ph <davidphang431@gmail.com>
2021-05-30 15:06:29 +0700

    Add continuous calculating and basic keyboard support

--- script.js
+++ script.js
@@ -14,28 +14,37 @@ let equalButtonPressed = false;
 //              EVENT LISTENER
 // /////////////////////////////
 document.addEventListener('click', onButtonClick);
+document.addEventListener('keydown', onKeyDown);
 
 
 //              FUNCTION
 // /////////////////////////////
 function onButtonClick(e){  
-
     if(e.target.classList.contains('calculator-button')){
         if(e.target.classList.contains('input-digit')){
-            onDigitClick(e.target);
+            onDigitClick(e.target.innerText);
         }else if(e.target.classList.contains('input-operator')){
-            onOperatorClick(e.target);
+            onOperatorClick(e.target.innerText);
         }else if(e.target.classList.contains('input-enter')){
             onEnterClick();
         }else if(e.target.classList.contains('input-clear')){
-            clearEverything();
+            clearVariables();
+            clearDisplay();
         }
     }
-
+}
+function onKeyDown(e){
+    if(e.key >= 0 && e.key <= 9 || e.key === '.'){
+        onDigitClick(e.key);
+    }else if(e.key === '+' || e.key === '/' || e.key === '-' || e.key === '*'){
+        onOperatorClick(e.key);
+    }else if(e.key === 'Enter'){
+        onEnterClick();
+    }
 }
 
 function onDigitClick(e){
-    let clickedDigit = e.innerText;
+    let clickedDigit = e;
 
     if(!operator){
         if(clickedDigit === '.'){
@@ -59,22 +68,40 @@ function onDigitClick(e){
 }
 
 function onOperatorClick(e){
+    // so we can continue clicking the operator button
+    // and we can get continues calculating
+    if(!equalButtonPressed && firstInput && secondInput){
+        let firstNumber = parseFloat(firstInput);
+        let secondNumber = parseFloat(secondInput);
+        let currentResult = calculate(firstNumber, secondNumber, operator);
+        firstInput = currentResult;
+        secondInput = '';
+    }
+    // put this on the bottom
+    // otherwise everyime we clicked the operator button after a number
+    // it will change the operator
+    operator = e;
     changeDisplay('0');
-    operator = e.innerText;
-    console.log(operator);
 }
 
 function onEnterClick(){
-    let firstNumber = parseFloat(firstInput);
-    let secondNumber = parseFloat(secondInput);
-    let finalResult = calculate(firstNumber, secondNumber, operator);
-    changeDisplay(finalResult);
+    if(firstInput && secondInput && operator){
+        equalButtonPressed = true;
+        let firstNumber = parseFloat(firstInput);
+        let secondNumber = parseFloat(secondInput);
+        let finalResult = calculate(firstNumber, secondNumber, operator);
+        changeDisplay(finalResult);
+        clearVariables();
+    }
 }
 
-function clearEverything(){
+function clearVariables(){
     firstInput = '';
     secondInput = '';
     operator = '';
+    equalButtonPressed = false;
+}
+function clearDisplay(){
     changeDisplay('0');
 }
 
